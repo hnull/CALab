86a8ee01d77531284209981372e6272cc71688af
maxjayne98 <36724807+maxjayne98@users.noreply.github.com>
2019-02-27 11:56:28 +0330

    Final Session 1

--- IF_Stage.v
+++ IF_Stage.v
@@ -3,11 +3,11 @@ module IF_Stage
     input clk,
     input rst,
     output[31:0] PC,
-    output[31:0] Instruction  
+    output[31:0] Instruction3 
   );
-  wire [31:0] PC_in, PC_out;
-  Adder adder(PC_out, PC_in);
-  PC pc(clk, rst, PC_in, PC_out);
-  Instrcution instruction(PC_out, Instruction);
-  assign PC = PC_out;  
+  wire [31:0] PC_out, Adder_out;
+  Adder Adder(PC_out, Adder_out);
+  PC PC2(clk, rst, Adder_out, PC_out);
+  Instruction Instruction2(Adder_out, Instruction3);
+  assign PC = Adder_out;  
 endmodule
