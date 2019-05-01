f2ba325b1a73c7b8cc0f6dcb058aebb5c7048e51
maxjayne98 <36724807+maxjayne98@users.noreply.github.com>
2019-05-01 19:36:21 +0430

    correct file name to "Tahvile_MIPS"

--- Tahvile_Hazard/Address_Mapping.v
+++
@@ -1,8 +0,0 @@
-module Address_Mapping
-  (
-    input[31: 0] in,
-    output[31:0] out
-  );
-
-  assign out = in - 32'b00000000000000000000010000000000;
-endmodule
--- Tahvile_Hazard/sign_extend.v
+++
@@ -1,7 +0,0 @@
-module Sign_Extend(
-  input [15:0] in,
-  output [31:0] out
-);
-  assign out = (in[15])?({16'b1111111111111111,in}):({16'b0000000000000000,in});
-endmodule
-  
