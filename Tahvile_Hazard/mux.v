85507f88c5523447eca094ccd381df3b88651d91
maxjayne98 <36724807+maxjayne98@users.noreply.github.com>
2019-05-12 09:13:43 +0430

    add "tahvile_hazard" folder to git

---
+++ Tahvile_Hazard/mux.v
@@ -0,0 +1,26 @@
+module Mux32
+  (
+    input is_immediate,
+    input [31:0] inp1,inp2,
+    output [31:0] out
+  );
+  assign out = (is_immediate)?(inp2):(inp1);
+endmodule
+
+module Mux5
+  (
+    input is_immediate,
+    input [4:0] inp1,inp2,
+    output [4:0] out
+  );
+  assign out = (is_immediate)?(inp2):(inp1);
+endmodule
+
+module Mux9
+  (
+    input sel,
+    input [9:0] inp1, inp2,
+    output [9:0] out
+  );
+  assign out = (sel) ? (inp2) : (inp1);
+endmodule
