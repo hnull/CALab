4ae5c2cffa451ce17b042883c5b8d67bf0b371a8
maxjayne98 <36724807+maxjayne98@users.noreply.github.com>
2019-06-09 20:38:49 +0430

    repair chache (revert it)

--- Mem_Stage.v
+++ Mem_Stage.v
@@ -26,45 +26,76 @@ module Mem_Stage
     assign ALU_result = ALU_result_in;
     assign MEM_R_EN = MEM_R_EN_in;
     assign Dest = Dest_in;
+
     /*
     Data_Memory data_mem (.address(adr), .write_data(ST_val_in),
                            .mem_read(MEM_R_EN_in), .mem_write(MEM_W_EN_in), .clk(clk),
                            .rst(rst), .read_data(MEM_read_value));
     */
-    wire sram_write_command,sram_read_command;
-    wire [63:0] output_for_cache;
-    wire freeze;
-    assign sram_ready = freeze;
-    wire [31:0] mem_val_between;
-    wire sram_ready_for_cache;
+    // wire sram_write_command,sram_read_command;
+    // wire [63:0] output_for_cache;
+    // // assign sram_ready = freeze;
+    // wire [31:0] mem_val_between;
+    // wire sram_ready_for_cache;
 
-   cache_controller Cache (
-             .clk(clk),
-             .rst(rst),
-             .address(adr),
-             .wdata(ST_val_in),
-             .MEM_R_EN(MEM_R_EN_in),
-             .MEM_W_EN(MEM_W_EN_in),
-             .rdata(MEM_read_value),
-             .SRAM_mem_read(sram_read_command),
-             .SRAM_mem_write(sram_write_command),
-             .sram_rdata(output_for_cache),
-             .sram_ready(sram_ready_for_cache),
-             .freeze(freeze)
-     );
-   SRAM sram(
-				      .clk(clk),
-              .rst(rst),
-              .mem_wr_en(sram_write_command),
-              .mem_rd_en(sram_read_command),
-              .address(adr),
-              .write_data(ST_val_in),
-              .ready(sram_ready_for_cache),
-              .SRAM_DQ(SRAM_DQ),
-              .SRAM_ADDR(SRAM_ADDR),
-              .SRAM_WE_N(SRAM_WE_N),
-              .output_for_cache(output_for_cache)
-             );
+
+    wire [17:0] edited_address;
+    assign edited_address = adr[18:1];
+    wire [63:0] sram_output;
+    wire sram_ready,sram_mem_read,sram_mem_write,freeze;
+     SRAM sram(
+    	.clk(clk),
+    	.rst(rst),
+    	.mem_read(sram_mem_read),
+    	.mem_write(sram_mem_write),
+    	input [32:0] data,
+    	input [17:0] .address(edited_address),
+    	inout [15:0] .SRAM_DQ(SRAM_DQ),
+    	output reg [17:0] .SRAM_ADDR(SRAM_ADDR),
+    	output reg .SRAM_WE_N(SRAM_WE_N),
+    	output reg [63:0] .data_out(sram_output),
+    	output .ready(sram_ready)
+    );
+    module Cache(
+    	.clk(clk),
+    	.rst(rst),
+    	.address(edited_address[16:1]),
+    	.mem_read(MEM_R_EN_in),
+    	.mem_write(MEM_W_EN_in),
+    	input [63:0] .SRAM_data(sram_output),
+    	input .SRAM_ready(sram_ready),
+    	output [31:0] .data(MEM_read_value),
+    	output reg .SRAM_mem_read(sram_mem_read),
+    	output reg .SRAM_mem_write(sram_mem_write),
+    	output .freeze(freeze)
+    );
+   // cache_controller Cache (
+   //           .clk(clk),
+   //           .rst(rst),
+   //           .address(adr),
+   //           .wdata(ST_val_in),
+   //           .MEM_R_EN(MEM_R_EN_in),
+   //           .MEM_W_EN(MEM_W_EN_in),
+   //           .rdata(MEM_read_value),
+   //           .SRAM_mem_read(sram_read_command),
+   //           .SRAM_mem_write(sram_write_command),
+   //           .sram_rdata(output_for_cache),
+   //           .sram_ready(sram_ready_for_cache),
+   //           .freeze(freeze)
+   //   );
+   // SRAM sram(
+		// 		      .clk(clk),
+   //            .rst(rst),
+   //            .mem_wr_en(sram_write_command),
+   //            .mem_rd_en(sram_read_command),
+   //            .address(adr),
+   //            .write_data(ST_val_in),
+   //            .ready(sram_ready_for_cache),
+   //            .SRAM_DQ(SRAM_DQ),
+   //            .SRAM_ADDR(SRAM_ADDR),
+   //            .SRAM_WE_N(SRAM_WE_N),
+   //            .output_for_cache(output_for_cache)
+   //           );
 	 Address_Mapping address_mapping (.in(ALU_result_in), .out(adr));
   always @(posedge clk, posedge rst)
     begin
---
+++ sram_.v
@@ -0,0 +1,94 @@
+module SRAM(
+	input clk,
+	input rst,
+	input mem_read,
+	input mem_write,
+	input [32:0] data,
+	input [17:0] address,
+	inout [15:0] SRAM_DQ,
+	output reg [17:0] SRAM_ADDR,
+	output reg SRAM_WE_N,
+	output reg [63:0] data_out,
+	output ready
+);
+	reg [15:0] data_temp;
+	reg [2:0] count;
+	always @(posedge clk, posedge rst) begin
+		if(rst)
+			count = 3'b0;
+		else begin
+			if(mem_write) begin
+				SRAM_WE_N = 1'b1;
+				case(count)
+					3'b000 : begin
+						SRAM_ADDR = address;
+						data_temp = data[15:0];
+						SRAM_WE_N = 1'b0;
+						count = count + 1;
+					end
+					3'b001 : begin
+						SRAM_ADDR = address + 1;
+						data_temp = data[31:16];
+						SRAM_WE_N = 1'b0;
+						count = count + 1;
+					end
+					3'b101 : begin
+						count = count + 1;
+					end
+					default : begin
+						count = count + 1;
+						data_temp = 16'bZZZZ_ZZZZ_ZZZZ_ZZZZ;
+					end
+				endcase
+
+			end
+			else if(mem_read) begin
+				SRAM_WE_N = 1'b1;
+				case(count)
+					3'b000 : begin
+						SRAM_ADDR = address;
+						count = count + 1;
+					end
+					3'b001 : begin
+						SRAM_ADDR = address + 1;
+						data_out[15:0] = SRAM_DQ;
+						count = count + 1;
+					end
+					3'b010 : begin
+						SRAM_ADDR = address + 2;
+						data_out[31:16] = SRAM_DQ;
+						count = count + 1;
+					end
+					3'b011 : begin
+						SRAM_ADDR = address + 3;
+						data_out[47:32] = SRAM_DQ;
+						count = count + 1;
+					end
+					3'b100 : begin
+						data_out[63:48] = SRAM_DQ;
+						count = count + 1;
+					end
+					3'b101 : begin
+						count = count + 1;
+					end
+					default : begin
+						count = count + 1;
+					end
+				endcase
+
+			end
+			else begin
+				SRAM_WE_N = 1'b1;
+				count = 3'b0;
+			end
+
+			if(count == 3'b110) begin
+				count = 3'b0;
+			end
+		end
+	end
+
+	assign ready = (count < 3'b101)? 1'b0 : 1'b1;
+	assign SRAM_DQ = mem_write ? data_temp : 16'bZZZZ_ZZZZ_ZZZZ_ZZZZ;
+
+endmodule
